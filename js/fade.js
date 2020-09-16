745aa9ba65e7d9f4b5edf4d6531eaeb5e7d63f1f
Pranav-5501 <68681580+Pranav-5501@users.noreply.github.com>
2020-09-16 09:58:32 +0530

    jq

---
+++ js/fade.js
@@ -0,0 +1,9 @@
+$('#btnSlideUp').click(function() {
+   $('#slideMe').slideUp('slow');
+});
+$('#btnSlideDown').click(function() {
+   $('#slideMe').slideUp();
+});
+$('#btnSlideToggle').click(function() {
+   $('#slideMe').slideToggle('fast');
+});
